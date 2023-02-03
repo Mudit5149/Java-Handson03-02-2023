f964f7d84ccd4569c371c884144b21d521786750
Mudit5149 <86524837+Mudit5149@users.noreply.github.com>
2023-02-03 14:17:50 +0530

    Add files via upload

---
+++ Prime_numbers.java
@@ -0,0 +1,24 @@
+package hackerrank_Solution;
+
+public class Prime_numbers {
+
+	public static void main(String[] args) {
+		int num = 50, i;
+		System.out.println("\n Prime numbers upto 50 :\n");
+		for (i = 2; i <= num; i++) {
+			boolean a = true;
+			for (int j = 2; j <= i - 1; j++) {
+				if (i % j == 0) {
+					a = false;
+					break;
+				}
+			}
+			if (a == true) {
+				System.out.print(" " + i);
+			}
+		}
+		System.out.println();
+
+	}
+
+}
